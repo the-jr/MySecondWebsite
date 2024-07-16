c85a53df3e1fadf263871edf3f41fa6111c1df03
the-jr <98658519+the-jr@users.noreply.github.com>
2024-07-16 14:20:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1040,6 +1040,124 @@ stopPropagation()	Prevents further propagation of an event during event flow	Eve
 
 -----------------------------------------------------
 
+HTML Objects
+<a>
+<abbr>
+<address>
+<area>
+<article>
+<aside>
+<audio>
+<b>
+<base>
+<bdo>
+<blockquote>
+<body>
+<br>
+<button>
+<canvas>
+<caption>
+<cite>
+<code>
+<col>
+<colgroup>
+<datalist>
+<dd>
+<del>
+<details>
+<dfn>
+<dialog>
+<div>
+<dl>
+<dt>
+<em>
+<embed>
+<fieldset>
+<figcaption>
+<figure>
+<footer>
+<form>
+<head>
+<header>
+<h1> - <h6>
+<hr>
+<html>
+<i>
+<iframe>
+<img>
+<ins>
+<input> button
+<input> checkbox
+<input> color
+<input> date
+<input> datetime
+<input> datetime-local
+<input> email
+<input> file
+<input> hidden
+<input> image
+<input> month
+<input> number
+<input> password
+<input> radio
+<input> range
+<input> reset
+<input> search
+<input> submit
+<input> text
+<input> time
+<input> url
+<input> week
+<kbd>
+<label>
+<legend>
+<li>
+<link>
+<map>
+<mark>
+<menu>
+<menuitem>
+<meta>
+<meter>
+<nav>
+<object>
+<ol>
+<optgroup>
+<option>
+<output>
+<p>
+<param>
+<pre>
+<progress>
+<q>
+<s>
+<samp>
+<script>
+<section>
+<select>
+<small>
+<source>
+<span>
+<strong>
+<style>
+<sub>
+<summary>
+<sup>
+<table>
+<tbody>
+<td>
+<tfoot>
+<th>
+<thead>
+<tr>
+<textarea>
+<time>
+<title>
+<track>
+<u>
+<ul>
+<var>
+<video>
 
 
 */
